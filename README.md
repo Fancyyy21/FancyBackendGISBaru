c3bc1ef93bd78dcdafaf763467c0e07d377d1734
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-11-27 18:47:50 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# fancy
+fancy
+
+
+```sh
+go get -u all
+go mod tidy
+git tag                                 #check current version
+git tag v1.28                          #set tag version
+git push origin --tags                  #push tag version to repo
+go list -m github.com/Fancyyy21/BackendGIS@v1.28   #publish to pkg dev, replace ORG/URL with your repo URL
+```
---
+++ mongo.go
@@ -0,0 +1,205 @@
+package peda
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"context"
+	"fmt"
+
+	"github.com/aiteung/atdb"
+	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SetConnection(mongoenv, dbname string) *mongo.Database {
+	var DBmongoinfo = atdb.DBInfo{
+		DBString: os.Getenv(mongoenv),
+		DBName:   dbname,
+	}
+	return atdb.MongoConnect(DBmongoinfo)
+}
+
+func SetConnectionTest(mongoenv, dbname string) *mongo.Database {
+	var DBmongoinfo = atdb.DBInfo{
+		DBString: mongoenv,
+		DBName:   dbname,
+	}
+	return atdb.MongoConnect(DBmongoinfo)
+}
+func GetAllBangunanLineString(mongoconn *mongo.Database, collection string) []GeoJson {
+	lokasi := atdb.GetAllDoc[[]GeoJson](mongoconn, collection)
+	return lokasi
+}
+
+func GetAllProduct(mongoconn *mongo.Database, collection string) []Product {
+	product := atdb.GetAllDoc[[]Product](mongoconn, collection)
+	return product
+}
+
+func GetNameAndPassowrd(mongoconn *mongo.Database, collection string) []User {
+	user := atdb.GetAllDoc[[]User](mongoconn, collection)
+	return user
+}
+
+func GetAllUser(mongoconn *mongo.Database, collection string) []User {
+	user := atdb.GetAllDoc[[]User](mongoconn, collection)
+	return user
+}
+func CreateNewUserRole(mongoconn *mongo.Database, collection string, userdata User) interface{} {
+	// Hash the password before storing it
+	hashedPassword, err := HashPassword(userdata.Password)
+	if err != nil {
+		return err
+	}
+	userdata.Password = hashedPassword
+
+	// Insert the user data into the database
+	return atdb.InsertOneDoc(mongoconn, collection, userdata)
+}
+func CreateUserAndAddedToeken(PASETOPRIVATEKEYENV string, mongoconn *mongo.Database, collection string, userdata User) interface{} {
+	// Hash the password before storing it
+	hashedPassword, err := HashPassword(userdata.Password)
+	if err != nil {
+		return err
+	}
+	userdata.Password = hashedPassword
+
+	// Insert the user data into the database
+	atdb.InsertOneDoc(mongoconn, collection, userdata)
+
+	// Create a token for the user
+	tokenstring, err := watoken.Encode(userdata.Username, os.Getenv(PASETOPRIVATEKEYENV))
+	if err != nil {
+		return err
+	}
+	userdata.Token = tokenstring
+
+	// Update the user data in the database
+	return atdb.ReplaceOneDoc(mongoconn, collection, bson.M{"username": userdata.Username}, userdata)
+}
+
+func DeleteUser(mongoconn *mongo.Database, collection string, userdata User) interface{} {
+	filter := bson.M{"username": userdata.Username}
+	return atdb.DeleteOneDoc(mongoconn, collection, filter)
+}
+func ReplaceOneDoc(mongoconn *mongo.Database, collection string, filter bson.M, userdata User) interface{} {
+	return atdb.ReplaceOneDoc(mongoconn, collection, filter, userdata)
+}
+func FindUser(mongoconn *mongo.Database, collection string, userdata User) User {
+	filter := bson.M{"username": userdata.Username}
+	return atdb.GetOneDoc[User](mongoconn, collection, filter)
+}
+
+func FindUserUser(mongoconn *mongo.Database, collection string, userdata User) User {
+	filter := bson.M{
+		"username": userdata.Username,
+	}
+	return atdb.GetOneDoc[User](mongoconn, collection, filter)
+}
+
+func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata User) bool {
+	filter := bson.M{"username": userdata.Username}
+	res := atdb.GetOneDoc[User](mongoconn, collection, filter)
+	return CheckPasswordHash(userdata.Password, res.Password)
+}
+
+// product
+
+func CreateNewProduct(mongoconn *mongo.Database, collection string, productdata Product) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, productdata)
+}
+
+func InsertUserdata(MongoConn *mongo.Database, username, role, password string) (InsertedID interface{}) {
+	req := new(User)
+	req.Username = username
+	req.Password = password
+	req.Role = role
+	return InsertOneDoc(MongoConn, "user", req)
+}
+func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID interface{}) {
+	insertResult, err := db.Collection(collection).InsertOne(context.TODO(), doc)
+	if err != nil {
+		fmt.Printf("InsertOneDoc: %v\n", err)
+	}
+	return insertResult.InsertedID
+}
+
+// gis function
+
+func PostPoint(mongoconn *mongo.Database, collection string, pointdata GeoJsonPoint) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, pointdata)
+}
+
+func PostLinestring(mongoconn *mongo.Database, collection string, linestringdata GeoJsonLineString) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, linestringdata)
+}
+
+func PostPolygon(mongoconn *mongo.Database, collection string, polygondata GeoJsonPolygon) interface{} {
+	return atdb.InsertOneDoc(mongoconn, collection, polygondata)
+}
+
+func MemasukkanKoordinat(MongoConn *mongo.Database, colname string, coordinate []float64, name, volume, tipe string) (InsertedID interface{}) {
+	req := new(Coordinate)
+	req.Type = tipe
+	req.Coordinates = coordinate
+	req.Name = name
+
+	ins := atdb.InsertOneDoc(MongoConn, colname, req)
+	return ins
+}
+
+func PostStructWithToken[T any](tokenkey string, tokenvalue string, structname interface{}, urltarget string) (result T, errormessage string) {
+	client := http.Client{}
+	mJson, _ := json.Marshal(structname)
+	req, err := http.NewRequest("POST", urltarget, bytes.NewBuffer(mJson))
+	if err != nil {
+		errormessage = "http.NewRequest Got error :" + err.Error()
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add(tokenkey, tokenvalue)
+	resp, err := client.Do(req)
+	if err != nil {
+		errormessage = "client.Do(req) Error occured. Error is :" + err.Error()
+		return
+	}
+	defer resp.Body.Close()
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		errormessage = "Error Read Data data from request." + err.Error()
+		return
+	}
+	if er := json.Unmarshal(respBody, &result); er != nil {
+		errormessage = string(respBody) + "Error Unmarshal from Response : " + er.Error()
+	}
+	return
+}
+
+// --------------------------------------------------------------------- START GIS 9 ---------------------------------------------------------------------
+
+func GeoIntersects(mongoconn *mongo.Database, long float64, lat float64) (namalokasi string) {
+	lokasicollection := mongoconn.Collection("gis")
+	filter := bson.M{
+		"geometry": bson.M{
+			"$geoIntersects": bson.M{
+				"$geometry": bson.M{
+					"type":        "Point",
+					"coordinates": []float64{long, lat},
+				},
+			},
+		},
+	}
+	var lokasi Lokasi
+	err := lokasicollection.FindOne(context.TODO(), filter).Decode(&lokasi)
+	if err != nil {
+		log.Printf("GeoIntersects: %v\n", err)
+	}
+	return lokasi.Properties.Name
+
+}
